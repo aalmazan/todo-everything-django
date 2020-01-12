6c094a617804df6bc161cd64abdd167ecd751b42
Allan Almazan <allan.almazan@gmail.com>
2020-01-11 21:29:04 -0800

    Add Todo model, test files, mixins

--- todo_everything/apps/todos/models.py
+++ todo_everything/apps/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from model_utils.models import SoftDeletableModel, TimeStampedModel
+from todo_everything.mixins.models import common
 
-# Create your models here.
+
+class Todo(models.Model, common.UserStampedModel, TimeStampedModel, SoftDeletableModel):
+    """Main model that contains todo data."""
+
+    title = models.CharField(max_length=100, blank=True)
+    body = models.TextField(blank=True)
