05d8d9ef7a2a7dd9078a4dd7f631c53f48629a2d
Allan Almazan <allan.almazan@gmail.com>
2020-02-08 19:21:12 -0800

    Dockerize stuff * Fix path issues * Add basic Dockerfile and docker-compose setup

--- todo_everything/settings/settings_base.py
+++ todo_everything/settings/settings_base.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add path to main project dir. Prevents import errors due to missing PATHs
+sys.path.insert(0, os.path.dirname(os.path.join(BASE_DIR)))
 
 
 # Quick-start development settings - unsuitable for production
