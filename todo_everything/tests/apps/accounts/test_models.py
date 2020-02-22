116f258a7f82c3f446b9ee82e753d2e9587e557b
Allan Almazan <allan.almazan@gmail.com>
2020-02-22 09:53:19 -0800

    Update to use model bakery

--- todo_everything/tests/apps/accounts/test_models.py
+++ todo_everything/tests/apps/accounts/test_models.py
@@ -1,14 +1,17 @@
 import pytest
+from model_bakery import baker
 from todo_everything.apps.accounts.models import Account
 
-from .factories import AccountFactory
+# def test_account_factory(account_factory):
+#     assert type(account_factory) == type(AccountFactory)
 
-
-def test_account_factory(account_factory):
-    assert type(account_factory) == type(AccountFactory)
+# @pytest.mark.django_db
+# def test_account_instance(account):
+#     """Test account instance works"""
+#     assert isinstance(account, Account)
 
 
 @pytest.mark.django_db
-def test_account_instance(account):
-    """Test account instance works"""
+def test_account_model_default_sanity():
+    account = baker.make(Account)
     assert isinstance(account, Account)
