78e291a75f6b75eb0906047097bd52dc96c72e6b
Allan Almazan <allan.almazan@gmail.com>
2020-02-22 09:52:52 -0800

    Add model bakery, initial model tests

--- todo_everything/apps/todos/models.py
+++ todo_everything/apps/todos/models.py
@@ -3,7 +3,7 @@ from model_utils.models import SoftDeletableModel, TimeStampedModel
 from todo_everything.mixins.models import common
 
 
-class Todo(models.Model, common.UserStampedModel, TimeStampedModel, SoftDeletableModel):
+class Todo(common.UserStampedModel, TimeStampedModel, SoftDeletableModel, models.Model):
     """Main model that contains todo data."""
 
     title = models.CharField(max_length=100, blank=True)
---
+++ todo_everything/tests/apps/todos/test_models.py
@@ -0,0 +1,9 @@
+import pytest
+from model_bakery import baker
+from todo_everything.apps.todos.models import Todo
+
+
+@pytest.mark.django_db
+def test_todo_model_default_sanity():
+    """Test that the Todo model default case is fine."""
+    assert isinstance(baker.make(Todo), Todo)
