430c50cfc9987cd866c083a11540dc67aa305a60
Allan Almazan <allan.almazan@gmail.com>
2023-12-21 17:02:50 -0800

    Add signal

---
+++ todo_everything/todos/signals.py
@@ -0,0 +1,14 @@
+import logging
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from . import models, tasks
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=models.Todo)
+def todo_creation(sender, **kwargs):
+    logger.info("Todo creation triggered")
+    tasks.notify.delay("todo_created")
